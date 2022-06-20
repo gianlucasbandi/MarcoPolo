d4402b2b9ece5ec593059b0d873ad0e2fbe8ee99
Mattia Russo <russo.1918968@studenti.uniroma1.it>
2022-06-20 10:49:03 +0200

    Finito il primo test

--- node/test/test.js
+++ node/test/test.js
@@ -1,3 +1,5 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';     //Disabilito la verifica del certificato (poiché è self-signed)
+
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 
@@ -5,10 +7,10 @@ chai.use(chaiHttp);
 var expect = chai.expect;
 
 describe("test that verify if the web app is on",function(){
-    it("should return status code 200",function(){
+    it("should return status code 200",function(done){
         chai.request("https://localhost:8083").get("/").end(function(err,res){
-            expect(err).to.equal("self signed certificate");
             expect(res).to.have.status(200);
+            done();
         });
     });
 });
