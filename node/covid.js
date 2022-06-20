9e3615abc8930bf7d23437bedd5423ebce799987
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2022-06-20 23:29:49 +0200

    covid.js prende dati covid in base alla nazione richiesta

--- node/covid.js
+++ node/covid.js
@@ -4,14 +4,25 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-app.get('/ita', (req, res) => {
-    res.send('https://corona.lmao.ninja/v2/countries/:query?strict=true')
-})
+app.get('/ita', function(req, res) {
+
+    request2server({
+        url: 'https://corona.lmao.ninja/v2/countries/italia?strict',
+        method: 'GET',
+    }, function(error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.send(response.statusCode + " " + body)
+            console.log(response.statusCode, body);
+        }
+    });;
+});
 
 app.get('/', (req, res) => {
-    res.send('Aoh je sta')
-})
+    res.send('Aoh je sta');
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+    console.log(`App listening on port ${port}`);
+});
