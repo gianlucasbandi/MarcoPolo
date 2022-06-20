4a58f5826fe31b5f04aed055a2f723c2e9e694d9
Mattia Russo <russo.1918968@studenti.uniroma1.it>
2022-06-20 18:36:29 +0200

    Aggiunta la view dalla pagina di index

--- node/index.js
+++ node/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const path = require('path');
+
 const PORT = 3000;
 const app = express();
 
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + '/views'));      //Usato per caricare i file statici (css)
+
 app.get("/",(req,res)=>{
-    const nodo = process.env.ISTANCE;
-    res.end("Ciao, sono il nodo "+nodo.toString());
+    res.render("index",{logged:false});
 });
+
+app.get("/login",(req,res)=>{
+    res.end("Cisanini");
+});
+
+
 
 app.listen(PORT,()=>{
     console.log("Applicazione in ascolto sulla porta "+PORT);
