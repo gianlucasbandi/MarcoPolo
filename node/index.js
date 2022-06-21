c632832c11c2cf3ed99349c571541db51165ca2f
Claudio Schiavella <schiavella.1884561@studenti.uniroma1.it>
2022-06-21 23:13:29 +0200

    Bozza di interfacciamento con api_covid+searchbar

--- node/index.js
+++ node/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+var request2server = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 const app = express();
@@ -7,18 +9,32 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-app.use(express.static(__dirname + '/views'));      //Usato per caricare i file statici (css)
+app.use(express.static(__dirname + '/views')); //Usato per caricare i file statici (css)
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-    res.render("index",{logged:false});
+app.get("/", (req, res) => {
+    res.render("index", { logged: false });
 });
 
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.end("Cisanini");
 });
 
+app.get('/nation', function(req, res) {
+    var tipo = req.originalUrl.split("=")[1];
+    request2server({
+        url: 'https://corona.lmao.ninja/v2/countries/' + tipo + '?strict',
+        method: 'GET',
+    }, function(error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.send(response.statusCode + " " + body)
+            console.log(response.statusCode, body);
+        }
+    });;
+});
 
-
-app.listen(PORT,()=>{
-    console.log("Applicazione in ascolto sulla porta "+PORT);
+app.listen(PORT, () => {
+    console.log("Applicazione in ascolto sulla porta " + PORT);
 });
