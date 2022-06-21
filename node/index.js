adb128467d204067c683d9a155fad9303acffef1
Mattia Russo <russo.1918968@studenti.uniroma1.it>
2022-06-21 19:01:40 +0200

    Terminata l'implementazione del login con Twitter, manca il logout

--- node/index.js
+++ node/index.js
@@ -1,12 +1,12 @@
 /***********DIPENDENZE*************/
 const express = require('express');
-const session = require('express-session')
+const cookieParser = require('cookie-parser');
 const path = require('path');
 const request = require('request');
 const qs = require('querystring');            //Per effettuare parsing delle query URL
 var Twit = require('twit');                  //Per gestire le richieste REST di Twitter
 require('dotenv').config();                 //Per ricavare i token necessari per OAuth
-
+const utils = require("./utils");
 
 const PORT = 3000;
 const app = express();
@@ -17,14 +17,6 @@ const twitterOAuth = {
     consumer_secret: TWITTER_CONSUMER_SECRET
 }
 
-/******SESSION*****/
-app.use(session({
-    secret: 'MarcoPoloSecret',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 24 * 60 * 60 * 1000, secure: true } // 1 giorno
-}));
-
 
 //Gestione delle views tramite pug
 app.set("views", path.join(__dirname, "views"));
@@ -32,32 +24,34 @@ app.set("view engine", "pug");
 app.use(express.static(__dirname + '/views'));      //Usato per caricare i file statici (css)
 
 
+app.use(cookieParser());
+
+
 /***********ROUTES*************/
 //Pagina iniziale
 app.get("/",(req,res)=>{
-    console.log(req.session.logged);
-    if(req.session.logged == undefined){     //La sessione non è settata; l'utente non è loggato
+    if(req.cookies.logged == undefined){     //Utente non loggato
         res.render("index",{logged:false});
     }
     
-    else if(req.session.user_name == undefined){     //Devo ancora completare la fase 3 di OAuth
+    else if(req.cookies.user_name == undefined){     //Devo ancora completare la fase 3 di OAuth
         //3 step : ottenimento token
         const req_data = qs.parse(req);
         var url = "https://api.twitter.com/oauth/access_token?oauth_token="+req.query.oauth_token+"&oauth_verifier="+req.query.oauth_verifier;
         request.post({url:url},(e,r,body)=>{
             const body_data = qs.parse(body);
-            req.session.oauth_token = body_data.oauth_token;
-            req.session.oauth_token_secret = body_data.oauth_token_secret;
-            req.session.user_id = body_data.user_id;
-            req.session.user_name = body_data.screen_name;
 
-            //res.render("index",{logged:true,username:req.session.user_name});
-            res.end(req.session.user_name);
+            utils.setCookie("oauth_token",body_data.oauth_token,res);
+            utils.setCookie("oauth_token_secret",body_data.oauth_token_secret,res);
+            utils.setCookie("user_id",body_data.user_id,res);
+            utils.setCookie("user_name",body_data.screen_name,res);
+    
+            res.render("index",{logged:true,username:body_data.user_name});
         });
     }
 
     else{                                   //L'utente era già loggato
-        res.end(req.session.user_name);
+        res.render("index",{logged:true,username:req.cookies.user_name});
     }
     
 });
@@ -72,8 +66,7 @@ app.get("/login",(req,res)=>{
         //2 step: autorizzazione resource owner
         url = "https://api.twitter.com/oauth/authorize?oauth_token="+req_data.oauth_token;
         request.get({uri:url},(e,r,body)=>{
-            req.session.logged = true;
-            console.log(req.session.logged);
+            utils.setCookie("logged",true,res);
             res.end(body);
         });
     });
---
+++ node/utils.js
@@ -0,0 +1,11 @@
+module.exports = {
+     setCookie : function(name,value,res){
+        res.cookie(name,value,{
+            maxAge: 60*60*24,       //Durata di un giorno
+            secure: true,
+            httpOnly: true,
+            sameSite: 'lax'         //Blocca il cross-site request
+        });
+    }
+}
+
