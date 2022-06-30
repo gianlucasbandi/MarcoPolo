006d9f45b939f0a111fe38c8b09f2304667fa81c
Mattia Russo <russo.1918968@studenti.uniroma1.it>
2022-06-30 12:31:43 +0200

    Modificato il css della chat

--- node/views/js/chatbot.js
+++ node/views/js/chatbot.js
@@ -2,15 +2,16 @@ $(document).ready(function (){
     var $chatbotUL = $("#chatbotUL");
     var $sendBtn =  $("#inputSubmit");
     var $inputMsg = $("#inputMsg");
-
+    var $chat = $(".chat");
 
     const ws = new WebSocket('wss://localhost:8083/chatbot');
 
 
     //Node send message via ws
     ws.addEventListener("message",function(event){
-        var $newItem = $("<li style=text-align:left;margin-left:0><p>"+event.data+"</p></li>");
+        var $newItem = $("<li class='bot-msg'><p>"+event.data+"</p></li>");
         $chatbotUL.append($newItem);
+        chatScrollDown();
     });
 
 
@@ -21,9 +22,18 @@ $(document).ready(function (){
         var msg = $inputMsg.val();
         if(msg != ""){
             ws.send(msg);
+            var $newItem = $("<li class='usr-msg'><p>"+msg+"</p></li>");
+            $chatbotUL.append($newItem);
+            chatScrollDown();
         }
 
         $inputMsg.val("");
     });
 
+
+    function chatScrollDown(){
+        $chat.animate({
+            scrollTop: $chat[0].scrollHeight
+        }, 1000);
+    }
 });
